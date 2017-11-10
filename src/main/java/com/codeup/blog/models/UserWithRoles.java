01bf5e9a91148dfdc79c77ea452f8e3cd7f386f4
michael <mgebregziabher86@gmail.com>
2017-11-10 14:34:59 -0600

    update resources package

---
+++ src/main/java/com/codeup/blog/models/UserWithRoles.java
@@ -0,0 +1,50 @@
+///*
+// * This source file is subject to the license that is bundled with this package in the file LICENSE.
+// */
+//package com.codeup.blog.models;
+//
+//import com.codeup.blog.services.UserDetailsLoader
+////import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.GrantAuthority
+//import org.springframework.security.core.authority.AuthorityUtils;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.util.StringUtils;
+//
+//import java.util.Collection;
+//import java.util.Collections;
+//
+//public class UserWithRoles extends User implements UserDetailsLoader {
+//    private Object AuthorityUtils;
+//
+//
+//    public UserWithRoles(User user) {
+//        super(user);  // Call the copy constructor defined in User
+//    }
+//
+//
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        String roles = StringUtils.collectionToCommaDelimitedString(Collections.emptyList());
+//        //return AuthorityUtils.commaSeparatedStringToAuthorityList("");
+//        return AuthorityUtils.commaSeparatedStringToAuthorityList(roles);
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return true;
+//    }
+//}
--- src/main/java/com/codeup/blog/repository/UsersRepository.java
+++ src/main/java/com/codeup/blog/repository/UsersRepository.java
@@ -1,8 +1,11 @@
-package com.codeup.blog.repository;
+package com.codeup.blog.repositories;
 
 import com.codeup.blog.models.User;
-
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UsersRepository extends CrudRepository <User, Long> {
+@Repository
+public interface UsersRepository extends CrudRepository<User, Long>{
+    // Query methods -> finder methods
+    User findByUsername(String username);
 }
